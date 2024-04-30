604266d763c3738c6364aef9e669ee418643b6dd
Braulio Zapata <brauliozapata@destiny.ws>
2024-04-30 16:32:55 -0400

    Add prod config

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
+        },
+        region: env("REGION_ENV_NAME"),
+      },
+    },
+  },
+});
